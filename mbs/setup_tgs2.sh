839f19d3356b0f8c886e4550d54ef87ba82b1f48
sakuramilk <c.sakuramilk@gmail.com>
2012-10-19 16:59:00 +0900

    update setup_tgs2.sh

--- mbs/setup_tgs2.sh
+++ mbs/setup_tgs2.sh
@@ -4,30 +4,29 @@ PROP_PATH=$1/tweakgs2.prop
 
 USB_CONFIG=`grep ro\.sys\.usb\.config $PROP_PATH | cut -d'=' -f2`
 if [ -n "$USB_CONFIG" ]; then
-    echo persist.sys.usb.config=$USB_CONFIG >> /default.prop
+    setprop persist.sys.usb.config $USB_CONFIG
 else
-    echo persist.sys.usb.config=mtp,adb >> /default.prop
+    setprop persist.sys.usb.config mtp,adb
 fi
 
 BOOT_SND=`grep audioflinger\.bootsnd $PROP_PATH | cut -d'=' -f2`
 if [ "$BOOT_SND" = '1' ]; then
-    echo audioflinger.bootsnd=1 >> /default.prop
+    setprop audioflinger.bootsnd 1
 else
-    echo audioflinger.bootsnd=0 >> /default.prop
+    setprop audioflinger.bootsnd 0
 fi
 
 CAMERA_SND=`grep ro\.camera\.sound\.forced $PROP_PATH | cut -d'=' -f2`
 if [ "$CAMERA_SND" = '1' ]; then
-    echo ro.camera.sound.forced=1 >> /default.prop
+    setprop ro.camera.sound.forced 1
 else
-    echo ro.camera.sound.forced=0 >> /default.prop
+    setprop ro.camera.sound.forced 0
 fi
 
 LCD_DENSITY=`grep ro\.sf\.lcd_density $PROP_PATH | cut -d'=' -f2`
-if [ -z "$LCD_DENSITY" ]; then
-    LCD_DENSITY=240
+if [ -n "$LCD_DENSITY" ]; then
+    setprop ro.sf.lcd_density $LCD_DENSITY
 fi
-echo ro.sf.lcd_density=$LCD_DENSITY >> /default.prop
 
 LOGGER=`grep ro\.tgs2\.logger $PROP_PATH | cut -d'=' -f2`
 if [ "$LOGGER" = '1' ] || [ -z "$LOGGER" ] ; then
@@ -47,5 +46,17 @@ fi
 J4FS=`grep ro\.tgs2\.j4fs $PROP_PATH | cut -d'=' -f2`
 if [ "$J4FS" = '1' ]; then
     insmod /lib/modules/j4fs.ko
+fi
+
+MUSIC_VOL_STEPS=`grep ro\.tweak\.music_vol_steps $PROP_PATH | cut -d'=' -f2`
+if [ -n "$MUSIC_VOL_STEPS" ]; then
+    setprop ro.tweak.music_vol_steps $MUSIC_VOL_STEPS
+fi
+
+SCROLLING_CACHE=`grep ro\.tweak\.scrolling_cache $PROP_PATH | cut -d'=' -f2`
+if [ "$SCROLLING_CACHE" = '1' ]; then
+    setprop ro.tweak.scrolling_cache 1
+else
+    setprop ro.tweak.scrolling_cache 0
 fi
 
